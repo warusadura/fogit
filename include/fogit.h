766587c312fa49c0d7d586e4c4fd2d2f994907a5
Dhanuka Warusadura <dhanuka@disroot.org>
2022-07-05 17:22:35 +0530

    Refoctors header files

--- include/fogit.h
+++ include/fogit.h
@@ -1,3 +1,7 @@
 /*
  * Copyright (C) 2022 Dhanuka Warusadura
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
--- src/main.c
+++ src/main.c
@@ -2,9 +2,6 @@
  * Copyright (C) 2022 Dhanuka Warusadura
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
